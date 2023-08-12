da5adee235ff9e04813402d240fd93fb4ec88693
R Edward Friedel <redwardweb@gmail.com>
2023-08-12 18:52:38 +0200

    initial working project structure

---
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/MvcRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcRecipesApplication.class, args);
+    }
+
+}
