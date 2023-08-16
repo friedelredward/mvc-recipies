d2eca7bd494618ac249fa07c2947c8159cc860e6
R Edward Friedel <redwardweb@gmail.com>
2023-08-16 14:51:38 +0200

    add: added  javax.persistence -> jakarta annotations. also relationship

--- mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Notes.java
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Notes.java
@@ -1,9 +1,43 @@
 package com.example.mvcrecipes.domain;
 
+import jakarta.persistence.*;
+
 /**
  * Created by r.edward on {16/08/2023}
  */
+@Entity
 public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /* here leave it as is (DOES NOT DELETE RECIPE ON DELETE)*/
+    @OneToOne
     private Recipe recipe;
+    /** so this string doesnt limit to 255 in the DB w allow much more.*/
+    @Lob
     private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
 }
--- mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Recipe.java
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Recipe.java
@@ -1,9 +1,16 @@
 package com.example.mvcrecipes.domain;
 
+import jakarta.persistence.*;
+
 /**
  * Created by r.edward on {16/08/2023}
+ * very important set this as entity
  */
+@Entity
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private Integer prepTime;
     private Integer cookTime;
@@ -13,6 +20,95 @@ public class Recipe {
     private String directions;
     //todo add
 //    private Difficulty difficulty;
+    /** this creates a blob field in the db
+     * to store larger objs(LargeOBject)*/
+    @Lob
     private Byte[] image;
+
+    /** this make this 1 the owner===
+     * WILL delete aLL notes if we delete 1 of this!!! Very Important!! */
+    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
 }
