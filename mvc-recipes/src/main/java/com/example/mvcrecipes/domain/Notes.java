6a9c59bb18dcd44f91fe722e1fd4334c9382226a
R Edward Friedel <redwardweb@gmail.com>
2023-08-16 14:38:49 +0200

    add: added notes and Recipe entities.

---
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Notes.java
@@ -0,0 +1,9 @@
+package com.example.mvcrecipes.domain;
+
+/**
+ * Created by r.edward on {16/08/2023}
+ */
+public class Notes {
+    private Recipe recipe;
+    private String recipeNotes;
+}
---
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/domain/Recipe.java
@@ -0,0 +1,18 @@
+package com.example.mvcrecipes.domain;
+
+/**
+ * Created by r.edward on {16/08/2023}
+ */
+public class Recipe {
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+//    private Difficulty difficulty;
+    private Byte[] image;
+    private Notes notes;
+}
