5724555e959e4721f4758af3a6ae4d8e899b178f
R Edward Friedel <redwardweb@gmail.com>
2023-08-18 16:55:26 +0200

    add: migrated panel to card and fixed banner and spring favicon.

--- mvc-recipes/src/main/java/com/example/mvcrecipes/controllers/IndexController.java
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/controllers/IndexController.java
@@ -1,14 +1,12 @@
 package com.example.mvcrecipes.controllers;
 
-import com.example.mvcrecipes.domain.Category;
-import com.example.mvcrecipes.domain.UnitOfMeasure;
 import com.example.mvcrecipes.repos.CategoryRepository;
 import com.example.mvcrecipes.repos.UnitOfMeasureRepository;
+import com.example.mvcrecipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * Created by r.edward on {12/08/2023}
@@ -18,19 +16,24 @@ import java.util.Optional;
 public class IndexController {
     private final CategoryRepository categoryRepository;
     private final UnitOfMeasureRepository  unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeService recipeService) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        log.debug("logging from index controller");
+    public String getIndexPage(Model model){
+/*        log.debug("logging from index controller");
         Optional<Category> categoryOptional= categoryRepository.findByDescription("American");
         Optional<UnitOfMeasure> uomOptional= unitOfMeasureRepository.findByDescription("Teaspoon");
         System.out.println("cat id is: " + categoryOptional.get().getId());
-        System.out.println("Unit id is: " + uomOptional.get().getId());
+        System.out.println("Unit id is: " + uomOptional.get().getId());*/
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
