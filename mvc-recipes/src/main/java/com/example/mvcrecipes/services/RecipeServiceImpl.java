86b334e3fe698473f2017451268427c3833f4ade
R Edward Friedel <redwardweb@gmail.com>
2023-08-19 21:03:55 +0200

    add: TDD: added view Recipe in recipe/show/{id} with link from table

--- mvc-recipes/src/main/java/com/example/mvcrecipes/services/RecipeServiceImpl.java
+++ mvc-recipes/src/main/java/com/example/mvcrecipes/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -28,5 +29,10 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Optional<Recipe> findById(Long id) {
+        return recipeRepository.findById(id);
     }
 }
